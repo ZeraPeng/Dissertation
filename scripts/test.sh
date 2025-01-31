6adf49c7fea18068dcbfcb36fae21067f1246ade
Zera Peng <dpm4212@outlook.com>
2025-01-31 15:13:01 +0800

    change .sh file to test.sh

--- scripts/test.sh
+++ scripts/test.sh
@@ -41,38 +41,6 @@ run_experiment() {
     )
     za=${r1:0-35:5} c=${r1:0-18:1}
     echo "Best ZSL Acc: $za on cycle $c"
-
-    echo "=========="
-    echo "Stage 2" # val
-    echo "..."
-    r2=$(
-        python train_2.py \
-            --num_classes $num_classes --ss "$ss" --st $st --ve $visual_encoder --le $language_encoder --tm $tm --num_cycles $nc --num_epoch_per_cycle $nepc \
-            --latent_size $ls --i_latent_size $ils --lr $lr --phase val --mode $mode --dataset_path "$edir" --wdir "$wdir_2" \
-            --dis_step $dis_step --batch_size $batch_size --dataset $dataset_local
-    )
-
-    echo "=========="
-    echo "Stage 3"
-    echo "..."
-    r3=$(
-        python gating_train.py \
-            --num_classes $num_classes --ss "$ss" --st $st --ve $visual_encoder --le $language_encoder --tm $tm --phase val --dataset_path "$edir" \
-            --wdir "$wdir_2" --th $th --t $t --dataset $dataset_local
-    )
-    echo "thresh: ${r3:0-23:4}, temp: ${r3:0-1}"
-
-    echo "=========="
-    echo "Stage 4"
-    echo "..."
-    r4=$(
-        python gating_eval.py \
-            --num_classes $num_classes --ss "$ss" --st $st --phase train --dataset_path "$tdir" --wdir "$wdir_1" --ve $visual_encoder --le $language_encoder --tm $tm \
-            --thresh "${r3:0-23:4}" --temp "${r3:0-1}" --dataset $dataset_local
-    )
-    sa=${r4:15:5} ua=${r4:39:5} hm=${r4:0-6:5}
-    echo "S_Acc: ${sa}, U_Acc: ${ua}, H_Mean: ${hm}"
-    echo "Finish"
 }
 
 for dataset_split in "${available_splits[@]}"; do
