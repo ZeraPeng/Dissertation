7d58203712161fc3a2ced969c1a669e4b5c3d8eb
ZeraPeng <dpm4212@outlook.com>
2025-04-16 13:02:34 +0800

    xprompt checked

--- ASKG/utils/check_files.py
+++ ASKG/utils/check_files.py
@@ -2,7 +2,7 @@ import yaml
 import os
 import ipdb
 
-def first_check():
+def askg_check():
     # File path configuration
     root_path = "/usr1/home/s124mdg53_04/Dissertation/ASKG"  # Set your root directory path here
     label_file = os.path.join(root_path, 'data/ntu', 'ntu120_label.txt')
@@ -25,5 +25,25 @@ def first_check():
     with open(output_file, 'w', encoding='utf-8') as f:
         yaml.dump(askg, f, allow_unicode=True, sort_keys=False, default_flow_style=False)
 
+def xprompt_check():
+    # File path configuration
+    root_path = "/usr1/home/s124mdg53_04/Dissertation/ASKG"  # Set your root directory path here
+    label_file = os.path.join(root_path, 'data/ntu', 'ntu120_label.txt')
+    xprompt_file = os.path.join(root_path, 'data/ntu', "classes_xprompt_ntu.yml")
+    output_file = os.path.join(root_path, 'data/ntu', "classes_xprompt_ntu_checked.yml")
+
+    with open(label_file, 'r') as f:
+        labels = [line.strip() for line in f.readlines() if line.strip()]
+    with open(xprompt_file, 'r') as f:
+        xprompt = yaml.load(f, Loader=yaml.FullLoader)
+    ipdb.set_trace()
+    for i, item in enumerate(xprompt):
+        for _, key in enumerate(item):
+            for j, label in enumerate(labels):
+                if  key == label:
+                    item[key]['idx'] = j
+    with open(output_file, 'w', encoding='utf-8') as f:
+        yaml.dump(xprompt, f, allow_unicode=True, sort_keys=False, default_flow_style=False)
+
 if __name__ == "__main__":
-    main()
+    xprompt_check()
